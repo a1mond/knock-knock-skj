9a24de36eb685998e1562352422e15f7473bfd2c
a1mond <kotik.slava2012@gmail.com>
2021-05-31 02:20:35 +0200

    final adjustments

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 public class Main {
-    private static final int[] port_arr = {1355, 1390, 1399, 1358};
 
-    private static final int port_from = 1350, port_to = 1400;
+    public static void main(String[] args) throws InvalidArgumentsException {
+        int port_server;
+        int port_from;
+        int port_to;
+        try {
+            port_server = Integer.parseInt(args[0]);
+            port_from = Integer.parseInt(args[1]);
+            port_to = Integer.parseInt(args[2]);
+        } catch (IndexOutOfBoundsException e) {
+            throw new InvalidArgumentsException("Invalid or no arguments have been provided, the format should be\n" +
+                    "\"*server_port*, *knock_port_from*, *knock_port_to*\"");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid argument have been provided, consider using only numbers");
+        }
 
-    public static void main(String[] args) {
-        for (int i : port_arr)
-            new ServerRunner(new UDPServer(i));
+        new ServerRunner(new UDPServer(port_server));
 
         for (int port = port_from; port < port_to; port++)
             new Client().start(port);
+        System.out.println("Knocking is finished");
+        System.exit(0);
     }
 }
